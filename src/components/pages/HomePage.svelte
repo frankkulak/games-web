3ce225ed66a11591f2a2e6c2c2ffa3a4adca0805
Frank Kulak <fs.kulak@gmail.com>
2022-07-13 10:07:37 -0400

    Improve home page.

--- src/components/pages/HomePage.svelte
+++ src/components/pages/HomePage.svelte
@@ -6,7 +6,20 @@
   <title>Tiny Games</title>
 </svelte:head>
 <section>
-  <a href="#/minesweeper" class="text-black dark:text-white">Minesweeper</a>
+  <div class="w-60">
+    <a href="#/minesweeper">
+      <div class="rounded-lg bg-gray-100 dark:bg-gray-700 shadow-lg w-60">
+        <img
+          src="./assets/minesweeper.png"
+          alt="Minesweeper"
+          class="rounded-t-lg w-60"
+        />
+        <div class="py-2 w-full text-center">
+          <h3 class="text-black dark:text-white font-bold">Minesweeper</h3>
+        </div>
+      </div>
+    </a>
+  </div>
 </section>
 
 <style lang="scss">
